43cfaf18de130be86135ce77cf554beb9da1b7a0
Erik Larson <109100846+erik-larson01@users.noreply.github.com>
2026-04-13 18:15:57 -0500

    Add: eating-bananas - submission-8

---
+++ Data Structures & Algorithms/eating-bananas/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # Define the range of what k can be
+        minK, maxK = 1, max(piles)
+        result = maxK
+
+        while minK <= maxK:
+            midK = (minK + maxK) // 2
+            totalTime = 0
+            for pile in piles:
+                # Time taken is ceil(x / k) as Koko finishes the full hour
+                totalTime += math.ceil(float(pile) / midK)
+            if totalTime <= h:
+                maxK = midK - 1
+                result = midK
+            else:
+                minK = midK + 1
+
+        return result
