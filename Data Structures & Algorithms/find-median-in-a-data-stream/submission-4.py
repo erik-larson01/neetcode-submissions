f3599fc9cb3e899c825a45d8722e99755a6f2e86
Erik Larson <109100846+erik-larson01@users.noreply.github.com>
2026-04-20 22:03:31 -0500

    Add: find-median-in-a-data-stream - submission-4

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-4.py
@@ -0,0 +1,33 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.small = []
+        self.large = []
+
+    def addNum(self, num: int) -> None:
+        # Add to either min heap and max heap
+        if self.large and num > self.large[0]:
+            heapq.heappush(self.large, num)
+        else:
+            heapq.heappush(self.small, -1 * num)
+
+        # Rebalance (median is the top of either heap)
+        # If one heap is larger than the other by more than 1, move top element to other heap
+        if len(self.small) - len(self.large) >= 1:
+            # Move top of small to large
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        elif len(self.large) - len(self.small) >= 1:
+            # Move top of large to small
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        # If sizes differ (odd total elements) return top of larger heap
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        elif len(self.large) > len(self.small):
+            return self.large[0]
+
+        # Sizes are even - return mean of two tops
+        return (-1 * self.small[0] + self.large[0]) / 2.0
