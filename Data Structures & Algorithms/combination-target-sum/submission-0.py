1204f2df8e594be63b1714eccdd4cfb9fbb5c8a6
Erik Larson <109100846+erik-larson01@users.noreply.github.com>
2026-04-22 18:48:45 -0500

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        def dfs(i, currentList, total):
+            # If the target has been reached, add the combination to the result array
+            if total == target:
+                res.append(currentList.copy())
+                return
+            # Stop exploring the decision tree
+            if i == len(nums) or total > target:
+                return
+            
+            # Include the current number (i does not change so the number can be reused)
+            currentList.append(nums[i])
+            dfs(i, currentList, total + nums[i])
+            # Backtrack: Exclude the current number
+            currentList.pop()
+            dfs(i + 1, currentList, total)
+        dfs(0, [], 0)
+        return res
