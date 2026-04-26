d40fdf5a6ad353f02126a3240824b48eea81cc3a
Erik Larson <109100846+erik-larson01@users.noreply.github.com>
2026-04-26 12:33:28 -0500

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        stack = []
+        
+        # Track openNum and closedNum to ensure complete parentheses
+        def backtrack(openNum, closedNum):
+            if openNum == closedNum == n:
+                # Valid parenthesis, save to res
+                res.append("".join(stack))
+                return
+            
+            if openNum < n:
+                # A "(" can be added
+                stack.append("(")
+                backtrack(openNum + 1, closedNum)
+                stack.pop()
+            if closedNum < openNum: # Parentheses need to be completed
+                # A ")" can be added
+                stack.append(")")
+                backtrack(openNum, closedNum + 1)
+                stack.pop()
+
+        backtrack(0,0)
+        return res    
