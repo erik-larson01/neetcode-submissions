ac83ae6db7ff38c8f4cff8b6f937cc96e405b78d
Erik Larson <109100846+erik-larson01@users.noreply.github.com>
2026-06-06 17:31:40 -0400

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def backtrack(nums, currPerm, isPicked):
+            if len(nums) == len(currPerm):
+                res.append(currPerm.copy())
+                return
+            for i in range(len(nums)):
+                if not isPicked[i]:
+                    currPerm.append(nums[i])
+                    isPicked[i] = True
+
+                    # backtrack
+                    backtrack(nums, currPerm, isPicked)
+                    currPerm.pop()
+                    isPicked[i] = False
+
+        backtrack(nums, [], [False] * len(nums))
+        return res
