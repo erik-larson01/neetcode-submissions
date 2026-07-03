6a74994e874a74e6ae2630ab5a55440ce4c5cd51
Erik Larson <109100846+erik-larson01@users.noreply.github.com>
2026-07-02 21:28:33 -0400

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # dp[i] = # of combinations for s[i:]
+        dp = [0] * (len(s) + 1)
+        dp[len(s)] = 1
+
+        # Loop backwards
+        for i in range(len(s) - 1, -1, -1):
+            # Check for invalid zeroes 
+            # (a string starting with zero cannot be decoded on its own)
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                # Take one digit
+                dp[i] = dp[i + 1]
+             
+            # Take two digits
+            if i < len(s) - 1 and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"):
+                dp[i] += dp[i + 2]
+        
+        return dp[0]
