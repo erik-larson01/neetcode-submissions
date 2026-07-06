532f6929f9a47bf0fa4cadd63922f1b5dfe5cc77
Erik Larson <109100846+erik-larson01@users.noreply.github.com>
2026-07-06 19:48:48 -0400

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = nums[0]
+        currMax = 1 # So first multiplication does not change first num
+        currMin = 1
+
+        # Choose between staring over and extending prev best
+        for num in nums:
+            temp = currMax * num # Need old value when calc currMin
+            currMax = max(num * currMax, num * currMin, num)
+            currMin = min(temp, num * currMin, num)
+            res = max(res, currMax)
+        return res
