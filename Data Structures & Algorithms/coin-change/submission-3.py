ccf275108c9a383bb15c58ff8d2802311b178834
Erik Larson <109100846+erik-larson01@users.noreply.github.com>
2026-07-08 19:59:27 -0400

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.py
@@ -0,0 +1,16 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+
+        # DP[i] is the minumum # of coins to make amount i
+        # Result at DP[amount]
+
+        dp[0] = 0
+
+        for amnt in range(1, amount + 1):
+            for c in coins: # Try every coin
+                if amnt - c >= 0:
+                    # If the coin is able to be taken, use prev result
+                    # We have already solved amnt - c
+                    dp[amnt] = min(dp[amnt], 1 + dp[amnt - c])
+        return dp[amount] if dp[amount] != amount + 1 else -1
